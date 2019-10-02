b5f795bf428d956108c548f2447da8993e4a45c5
App Academy Student <student@appacademy.io>
2019-10-01 17:38:05 -0700

    Added final music files

--- W7D2/music_app/app/controllers/bands_controller.rb
+++ W7D2/music_app/app/controllers/bands_controller.rb
@@ -2,12 +2,13 @@ class BandsController < ApplicationController
  
 
   def index
-    @bands= Band.all 
+    @bands = Band.all 
     render :index
   end
 
   def show
-    
+    @band = Band.find(params[:id])
+    render :show
   end
 
   def new
@@ -15,22 +16,36 @@ class BandsController < ApplicationController
   end
 
   def create
-
+    band = Band.new(band_params)
+    if band.save
+      redirect_to band_url(band)
+    else
+      render :new
+    end
   end
 
   def edit
-  
+    @band = Band.find(params[:id])
+    render :edit
   end
 
   def update
-
+    band = Band.find(params[:id])
+    if band.update(band_params)
+      redirect_to band_url(band)
+    else
+      redirect_to edit_band_url(band)
+    end
   end
     
   def destroy 
-
+    band = Band.find(params[:id])
+    band.destroy
+    redirect_to bands_url
   end
 
   private
+
   def band_params
     params.require(:band).permit(:name)
   end
--- W7D2/music_app/db/schema.rb
+++ W7D2/music_app/db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_01_223531) do
+ActiveRecord::Schema.define(version: 2019_10_01_233128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "albums", force: :cascade do |t|
+    t.integer "band_id", null: false
+    t.string "title", null: false
+    t.integer "year", null: false
+    t.boolean "studio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "bands", force: :cascade do |t|
     t.string "name", null: false
